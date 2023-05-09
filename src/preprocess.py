9b3c149314d56393e6f32e430416a8c2aafe2658
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2023-05-09 13:14:07 +0300

    fx

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,23 +6,20 @@ from sklearn.model_selection import train_test_split
 import sys
 import traceback
 
-from kafka_consumer import Consumer
-from kafka_producer import Producer
 from logger import Logger
 
 TEST_SIZE = 0.2
 SHOW_LOG = True
 
 
-class DataMaker:
+class DataMaker():
 
     def __init__(self) -> None:
         logger = Logger(SHOW_LOG)
         self.config = configparser.ConfigParser()
         self.log = logger.get_logger(__name__)
         self.project_path = os.path.join(os.getcwd(), "data")
-        self.kafka_producer = Producer()
-        self.kafka_consumer = Consumer()
+        self.data_path = os.path.join(self.project_path, "penguins_size.csv")
         self.X_path = os.path.join(self.project_path, "penguins_X.csv")
         self.y_path = os.path.join(self.project_path, "penguins_y.csv")
         self.train_path = [os.path.join(self.project_path, "Train_penguins_X.csv"), os.path.join(
@@ -32,14 +29,9 @@ class DataMaker:
         self.log.info("DataMaker is ready")
 
     def get_data(self) -> bool:
-        self.kafka_producer.update_data(1000)
-        self.kafka_consumer.observe_data()
-        dataset = self.kafka_consumer.get_data()
-        dataset["sex"] = dataset["sex"] \
-            .replace(np.nan, "NO_GENDER") \
-            .replace('.', "NO_GENDER") \
-            .replace("nan", "NO_GENDER")
-        dataset = dataset.replace(np.nan, 0).replace("nan", 0)
+        dataset = pd.read_csv(self.data_path)
+        dataset["sex"] = dataset["sex"].replace(np.nan, "NO_GENDER").replace('.', "NO_GENDER")
+        dataset = dataset.replace(np.nan, 0)
         dataset = pd.get_dummies(dataset, columns=['sex', 'island'])
         X = pd.DataFrame(dataset.iloc[:, 1:].values)
         y = pd.DataFrame(dataset.iloc[:, 0].values)
